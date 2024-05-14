9d106c5f0ca4c1c7a06da1a2ed6d929f1c3b7408
rushanatownend <volen21@yahoo.com>
2024-05-14 12:01:53 +0100

    done with the seed

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many_attached :photos
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,78 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
+
+require 'faker'
+
+puts "Cleaning database..."
+Booking.destroy_all
+Venue.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+3.times do |i|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  username = "#{first_name.downcase}_#{last_name.downcase}_#{i + 1}"
+  email = Faker::Internet.email(name: "#{first_name} #{last_name}", separators: '.', domain: 'example.com')
+  password = 123123
+
+
+  User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    username: username,
+    email: email,
+    password: password
+  )
+end
+
+
+image_directory = "app/assets/images/"
+venue_images = ["meet.jpg", "work.jpg", "celebration.jpg", "21.jpg", "22.jpg", "23.jpg", "25.jpg", "27.jpg"]
+categories = [:network, :meet, :celebrate]
+facilities_array = [:wifi, :coffee, :tv, :pool, :beach, :spa]
+
+10.times do |i|
+  new_venue = Venue.new(
+    name: Faker::Restaurant.unique.name,
+    facilities: facilities_array.sample(3).map(&:to_s).join(", "),
+    address: Faker::Address.full_address,
+    category: categories.sample,
+    capacity: Faker::Number.between(from: 10, to: 100),
+    user_id: User.ids.sample
+  )
+
+  image_filename = venue_images.sample
+  image_path = File.join(image_directory, image_filename)
+
+#   if File.exist?(image_path)
+#     File.open(image_path) do |file|
+#       new_venue.photos.attach(io: file, filename: image_filename, content_type: "image/jpeg")
+#     end
+#   else
+#     puts "Image file not found: #{image_path}"
 #   end
-ed = User.new(first_name: "ed", last_name: "bob", username: "edbob", email: "ed@bob.com", password: "123123")
-ed.save
+
+#   new_venue.save
+# end
+  if File.exist?(image_path)
+
+    new_venue.photos.attach(io: File.open(image_path), filename: image_filename, content_type: "image/jpeg")
+  else
+    puts "Image file not found: #{image_path}"
+  end
+
+  new_venue.save
+end
+
+# puts "Creating bookings..."
+
+# 5.times do
+#   Booking.create!(
+#     comment: Faker::Lorem.sentence,
+#     confirmed: false,
+#     user_id: User.ids.sample,
+#     venue_id: Venue.ids.sample
+#   )
+# end
+
+puts "Seeding completed!"
