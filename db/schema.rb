3ca7097c173c69cdd0f47dabc140278ea6473d1e
rushanatownend <volen21@yahoo.com>
2024-05-15 17:38:04 +0100

    do not merge this- work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_220856) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_063442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +49,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_220856) do
     t.bigint "venue_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "booking_start"
+    t.date "booking_end"
+    t.date "start_date"
+    t.date "end_date"
     t.date "booking_date"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
