b12f5fe41609d84e387294bf004912096bc495da
rushanatownend <volen21@yahoo.com>
2024-05-16 14:54:12 +0100

    seed done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_191540) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_16_120132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_191540) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
