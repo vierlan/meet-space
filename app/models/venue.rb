945ade87bd50d380c55b067f8aabb891eb1a687d
rushanatownend <volen21@yahoo.com>
2024-05-20 13:32:57 +0100

    make links on home page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,21 +1,31 @@
 class Venue < ApplicationRecord
+  CATEGORIES = %w[networking social celebrate]
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true
+  # validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   validates :capacity, presence: true
   validates :description, length: { maximum: 1000 }, allow_blank: true
+
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_facilities,
-    against: [ :name, :facilities, :description ],
+    against: [ :name, :facilities, :description, :address ],
     using: {
       tsearch: { prefix: true }
     }
+end
+# venue.rb (model)
+class Venue < ApplicationRecord
+  CATEGORIES = %w[networking social celebration]
+
+  validates :category, inclusion: { in: CATEGORIES }
 end
