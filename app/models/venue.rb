5fb3d603f97e7d7f8f01d721bb15dd65d389295f
Lananh Nguyen <lananhnguyen@live.co.uk>
2024-05-20 14:21:58 +0100

    add

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,7 +7,7 @@ class Venue < ApplicationRecord
   validates :address, presence: true
   validates :category, presence: true
   validates :capacity, presence: true
-  validates :description, allow_blank: true
+  validates :description, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
