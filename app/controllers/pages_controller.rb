a69dd4f44f76aa4ad1840ad08d0639f6db906367
rushanatownend <volen21@yahoo.com>
2024-05-22 20:43:20 +0100

    added messages to bookings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,12 +8,18 @@ class PagesController < ApplicationController
   def profile
     @user = current_user
     @venues = @user.venues
-    @bookings = @user.bookings
+    # @bookings = @user.bookings
 
-    @requests = current_user.venues.map{|venue| venue.bookings}.flatten
+
+    @bookings = current_user.bookings.order(created_at: :desc)
+    @chatrooms = current_user.chatrooms
+    @requests = current_user.venues.flat_map { |venue| venue.bookings }.sort_by { |booking| booking.created_at }.reverse
+
+    # @requests = current_user.venues.map{|venue| venue.bookings}.flatten
 
     # all of the bookings where the current user owns the venue
     @venue = Venue.last
+
 
 
   end
